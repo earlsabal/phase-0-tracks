6175d62e1e4a9ced7b6cc54e8923ff1ee7ec2799
Earl Sabal <earlsabal@gmail.com>
2017-03-11 20:45:49 -0800

    Made pseudocode for hashes program

---
+++ ruby/hashes.rb
@@ -0,0 +1,12 @@
+=begin
+- Greet the user
+- Ask for the client's name
+- Ask for the client's age
+- Ask for the number of children
+- Ask for the decor theme
+- Ask if there are pets
+- Print the information received
+- Ask if there are any mistakes
+- If user says yes, allow to fix that specific mistake
+- Print the final information
+=end
