abb7b06775809e9b5185ce856813777495dbc50c
Earl Sabal <earlsabal@gmail.com>
2017-03-12 01:29:12 -0800

    Made layout for nested structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,25 @@
+def build_nested_structure
+	michaels_store = {
+		adhesives: [],
+		baking: []
+		floral: []
+		fine_arts: {
+			drawing: []
+			painting: []
+			surfaces: []
+		}
+		framing: []
+		jewelry: []
+		kids: {
+			crayola: []
+			toys: []
+			general_crafts: []
+		}
+		scrapbooking: []
+		wedding: []
+		wood: []
+		yarn: []
+	}
+	michaels_store
+end
+
