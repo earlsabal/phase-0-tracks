e11aab2da1c9479c86bfa79efba5044a399bdb60
Earl Sabal <earlsabal@gmail.com>
2017-03-14 19:32:57 -0700

    Made pseudocode for program

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+=begin
+- takes first and last name and separate them into two different strings
+- turn strings into arrays of letters
+- starting from the first letter, change current letter to the next letter in alphabet
+	- if consonant, change to next consonant
+	- if vowel, change to next vowel
+- create a new string for new name
+- join the two arrays into two strings
+- insert the changed last name first into the new string
+- insert the changed first name into the new string
+- print new name
+=end
