eac430352a12bbf3fe17fcb3167b9a46019f0921
Earl Sabal <earlsabal@gmail.com>
2017-03-17 13:18:05 -0700

    Created an insertion sort method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -8,12 +8,10 @@ def search_array(array, target_number)
 	end
 	nil
 end
-=begin
 
 arr = [42, 89, 23, 1]
 p search_array(arr, 1) == 3
 p search_array(arr, 24) == nil
-=end
 
 def fib(number_of_elements)
 	numbers = [0, 1]
@@ -30,3 +28,27 @@ end
 p fib(6)
 p fib(100)
 p fib(100).last == 218922995834555169026
+
+def insertion_sort(array)
+	current_index = 1
+	while current_index < array.length
+		current_number = array[current_index]
+		array.delete_at(current_index)
+		sorting_index = 0
+		while sorting_index <= current_index
+			if sorting_index == current_index
+				array.insert(sorting_index, current_number)
+			else
+				if current_number < array[sorting_index]
+					array.insert(sorting_index, current_number)
+					break
+				end
+			end
+			sorting_index += 1
+		end
+		current_index += 1
+	end
+	array
+end
+
+p insertion_sort([5, 2, 3, 4, 7, 1]) == [1, 2, 3, 4, 5, 7]
