ac9579067edade7306bcc1a8358e9eb096c52c70
Earl Sabal <earlsabal@gmail.com>
2017-03-17 12:46:20 -0700

    Created method for Fibonacci numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -8,18 +8,25 @@ def search_array(array, target_number)
 	end
 	nil
 end
+=begin
 
 arr = [42, 89, 23, 1]
 p search_array(arr, 1) == 3
 p search_array(arr, 24) == nil
+=end
 
 def fib(number_of_elements)
 	numbers = [0, 1]
 	prev_index = 0
-	for index 1...number_of_elements
-		numbers << prev_index + index
+	index = 1
+	while numbers.length < number_of_elements
+		numbers << numbers[prev_index] + numbers[index]
+		prev_index = index
+		index += 1
 	end
 	numbers
 end
 
-fib(6)
+p fib(6)
+p fib(100)
+p fib(100).last == 218922995834555169026
