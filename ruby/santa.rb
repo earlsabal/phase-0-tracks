35e5f32d1922a68f9291527058331a3ee5823f7f
Earl Sabal <earlsabal@gmail.com>
2017-03-17 22:36:37 -0700

    Added 3 getter methods, 1 setter method, and 2 attribute-changing methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -13,15 +13,56 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}"
 	end
-end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer_name)
+		reindeer_index = @reindeer_ranking[reindeer_name].index
+		@reindeer_ranking.delete_at(reindeer_index)
+		@reindeer_ranking << reindeer_name
+	end
+
+	def gender
+		@gender
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+end
+=begin
 santa = Santa.new
 santa.speak
 santa.eat_milk_and_cookies("chocolate chip cookie")
+=end
 
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-for number_of_santas 0..50
+for number_of_santas in 0..50
 	santas << Santa.new(example_genders.sample, example_ethnicities.sample)
 end
+
+age = 1
+santas.each do |santa|
+	celebrations = 0
+	while celebrations < age
+		santa.celebrate_birthday
+		celebrations += 1
+	end
+	puts "This santa is #{santa.age} years old and #{santa.ethnicity}"
+	age +=1
+end
+p santas.last.gender
+santas.last.gender = example_genders[0]
+p santas.last.gender
