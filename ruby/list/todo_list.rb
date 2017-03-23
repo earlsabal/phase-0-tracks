3223f3887eb6ed987a6e26c9cfc9ba6b9f0c8076
Earl Sabal <earlsabal@gmail.com>
2017-03-23 15:19:49 -0700

    Made the third test in todo_list pass

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -6,8 +6,13 @@ class TodoList
 	def get_items
 		@list
 	end
-	
+
 	def add_item(task)
 		@list << task
+	end
+
+	def delete_item(task)
+		index_to_delete = @list.index(task)
+		@list.delete_at(index_to_delete)
 	end
 end
