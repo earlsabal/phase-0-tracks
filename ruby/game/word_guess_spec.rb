ec62cb4930a8ba064a569ab8857a06ec5a8580d1
Earl Sabal <earlsabal@gmail.com>
2017-03-25 14:36:03 -0700

    Finished making test cases for game

--- ruby/game/word_guess_spec.rb
+++ ruby/game/word_guess_spec.rb
@@ -1,29 +1,33 @@
 require_relative 'word_guess'
 
 describe Word_guess do
-  let(:word_guess) { Word_guess.new }
+  let(:word_guess) { Word_guess.new("hello") }
 
   it "checks if the game meets requirements for a word" do
-    expect(calculator.add(3,4)).to eq 7
+    expect(word_guess.word).to eq "hello"
   end
 
   it "checks if the game creates the right amount of guesses" do
-    expect(calculator.subtract(7,1)).to eq 6
+    expect(word_guess.guesses).to eq 10
   end
 
   it "checks if the game won't count previous guesses" do
-    expect(calculator.multiply(2,3)).to eq 6
+    word_guess.guess("h")
+    guesses_after_first_guess = word_guess.guesses_left
+    word_guess.guess("h")
+    expect(word_guess.guesses == guesses_after_first_guess).to eq true
   end
 
   it "checks if the game will reveal guessed letters" do
-    expect(calculator.multiply(2,3)).to eq 6
+    word_guess.guess("h")
+    expect(word_guess.revealed_letters).to eq "h _ _ _ _"
   end
 
   it "checks if the game will work if the player loses" do
-    expect(calculator.divide(4,2)).to eq 2
+    expect(word_guess.end_game).to eq "Congratulations! You won!"
   end
 
   it "checks if the game will work if the player wins" do
-    expect(calculator.multiply(2,3)).to eq 6
+    expect(word_guess.end_game).to eq "HA HA HA YOUUU LOOOOSSSEEE!"
   end
 end
