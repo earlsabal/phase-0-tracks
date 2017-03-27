274a79a394a66e6c4426a59dca24947dbfb8a43b
Earl Sabal <earlsabal@gmail.com>
2017-03-27 15:09:38 -0700

    Created psuedocode for reversing a string in js

---
+++ js/explore.js
@@ -0,0 +1,23 @@
+/**
+Method that takes in a string and reverses it
+input: string
+steps:
+	- Take in the string
+	- Create a new empty string
+	- go through each letter of the original string 
+		- add it to the beginning of the new string
+	- return new string
+output: string
+**/
+
+function reverse(stringToReverse) {
+	var reversedString = ""
+	for (var i = stringToReverse.length - 1; i >= 0; i--) {
+		reversedString = reversedString + stringToReverse[i]
+	}
+	return reversedString
+}
+
+console.log(reverse("hello"))
+var newString = reverse("bootcamp")
+console.log(newString == "pmactoob")
