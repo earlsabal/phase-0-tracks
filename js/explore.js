420a58319a2e7873d3b628a3b6609b86c929dcc2
Earl Sabal <earlsabal@gmail.com>
2017-03-27 15:16:07 -0700

    Cleaned up syntax

--- js/explore.js
+++ js/explore.js
@@ -11,7 +11,7 @@ output: string
 **/
 
 function reverse(stringToReverse) {
-	var reversedString = ""
+	var reversedString = "";
 	for (var i = stringToReverse.length - 1; i >= 0; i--) {
 		reversedString = reversedString + stringToReverse[i];
 	}
