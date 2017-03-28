f492cab3b714246581220e3dd05c9d821e6b52b1
Earl Sabal <earlsabal@gmail.com>
2017-03-27 17:11:24 -0700

    Created pseudocode for a function to find the longest phrase

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+/*
+Function to find longest phrase:
+input: array
+steps:
+	- create a variable to record an index
+	- create a variable to record the longest length
+	- Iterate through each element in the array
+		- find the LENGTH of the element
+		- set the recorder to that index IF its the longest length 
+	- RETURN the string at the recorded index
+output: string
+*/
+
