ccc91bb2870cb24328c7390ea1ff5f205d4b15ab
Earl Sabal <earlsabal@gmail.com>
2017-04-04 02:08:30 -0700

    Added user interface for transactions

--- SQL_Ruby_project/spending.rb
+++ SQL_Ruby_project/spending.rb
@@ -13,7 +13,6 @@ create_spending_table = <<-SQL
 SQL
 
 db.execute(create_spending_table)
-p db.execute("SELECT * FROM spending")
 
 # Program can create a new user with current balance
 
@@ -28,7 +27,7 @@ if has_an_account == "no"
 	valid_name = false
 	while valid_name == false
 		user_name = gets.chomp
-		if valid_name_checker(db, user_name)
+		if !existing_name_checker(db, user_name)
 			valid_name = true
 		else
 			p "Username is taken, please try another"
@@ -38,7 +37,44 @@ if has_an_account == "no"
 	balance = gets.to_f
 	converted_balance = money_to_data(balance)
 	add_user(db, user_name, converted_balance)
+else
+	p "What is your username?"
+	valid_name = false
+	while valid_name == false
+		user_name = gets.chomp
+		if !existing_name_checker(db, user_name)
+			"That username does not exist, please try again"
+		else
+			valid_name = true
+		end
+	end
 end
+desired_task = 0
+while desired_task != 9
+	p "What would you like to do today? Enter number"
+	p "1: See current balance"
+	p "2: Enter income"
+	p "3: Enter expense"
+	p "4: See transactions"
+	p "9: Exit program"
+	desired_task = gets.to_i
+	if desired_task == 1
+		p "Here is your current balance:"
+	elsif desired_task == 2
+		p "Here is your new current balance:"
+	elsif desired_task == 3
+		p "Here is your new current balance:"
+	elsif desired_task == 4
+		p "Here are your transactions:"
+	elsif desired_task == 9
+		p "Exiting..."
+	else
+		p "Not a valid entry, please try again"
+	end
+end
+p "Thank you for using the spending app!"
+		
+
 
 
 # Program can check for an existing user using username and pin
--- SQL_Ruby_project/spending_methods.rb
+++ SQL_Ruby_project/spending_methods.rb
@@ -1,10 +1,10 @@
 INCLUDED = [[1]]
 
-def valid_name_checker(db, name)
+def existing_name_checker(db, name)
 	if db.execute("SELECT 1 FROM spending WHERE username=?", [name]) == INCLUDED
-		false
-	else
 		true
+	else
+		false
 	end
 end
 
