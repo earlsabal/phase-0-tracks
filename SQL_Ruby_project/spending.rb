3a13d2c2387aa1602c10efe9f2fc2194deb3acc2
Earl Sabal <earlsabal@gmail.com>
2017-04-04 00:17:31 -0700

    Added some psuedocode for program

--- SQL_Ruby_project/spending.rb
+++ SQL_Ruby_project/spending.rb
@@ -1,3 +1,4 @@
+# Create database with table
 require 'sqlite3'
 
 db = SQLite3::Database.new("spending.db")
@@ -5,9 +6,17 @@ db = SQLite3::Database.new("spending.db")
 create_spending_table = <<-SQL
   CREATE TABLE IF NOT EXISTS spending(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    username VARCHAR(255),
+    pin INT,
     current_balance INT
   )
 SQL
 
 db.execute(create_spending_table)
+
+# Program can create a new user with a pin and current balance
+# Program can check for an existing user using username and pin
+# Program can take an income and add it to the current balance
+# Program can take an expense and subtract it to the current balance
+# Program can record a transaction and store it
+# Program can print all transactions, or requested number of transactions
