c653751080c7b33d2e375dc220f86685a84b46ba
Earl Sabal <earlsabal@gmail.com>
2017-04-04 22:40:59 -0700

    Added validation for income and expenses, and the option to go back

--- SQL_Ruby_project/spending.rb
+++ SQL_Ruby_project/spending.rb
@@ -47,7 +47,7 @@ spacing
 puts "Welcome #{user_name}!"
 desired_task = 0
 while desired_task != 9
-	puts "What would you like to do today? Enter number"
+	puts "What would you like to do today? (Enter number)"
 	puts "1: See current balance"
 	puts "2: Enter income"
 	puts "3: Enter expense"
@@ -58,23 +58,49 @@ while desired_task != 9
 		puts "Here is your current balance:"
 		puts format_money(current_bal(db, user_name).to_s)
 	elsif desired_task == 2
-		puts "How much is the income?"
-		income = gets.to_f
-		puts "How did you get this income?"
-		source = gets.chomp
-		time = Time.now
-		record_transaction(db, user_name, income, source, time, true)
-		puts "Here is your new current balance:"
-		puts add_transaction(db, user_name, income, true)
+		puts "How much is the income? (Type amount or type back)"
+		income = 0.0
+		while income == 0.0
+			income = gets.chomp
+			if income == "back"
+				puts "Going back..."
+			else
+				income = income.to_f
+				if income == 0.0
+					puts "Not a valid entry, please try again"
+				end
+			end
+		end
+		if income != "back"
+			puts "How did you get this income?"
+			source = gets.chomp
+			time = Time.now
+			record_transaction(db, user_name, income, source, time, true)
+			puts "Here is your new current balance:"
+			puts add_transaction(db, user_name, income, true)
+		end
 	elsif desired_task == 3
-		puts "How much is the expense?"
-		expense = gets.to_f
-		puts "Where did your expense go to?"
-		source = gets.chomp
-		time = Time.now
-		record_transaction(db, user_name, expense, source, time, false)
-		puts "Here is your new current balance:"
-		puts add_transaction(db, user_name, expense, false)
+		puts "How much is the expense? (Type amount or type back)"
+		expense = 0.0
+		while expense == 0.0
+			expense = gets.chomp
+			if expense == "back"
+				puts "Going back..."
+			else
+				expense = expense.to_f
+				if expense == 0.0
+					puts "Not a valid entry, please try again"
+				end
+			end
+		end
+		if expense != "back"
+			puts "Where did your expense go to?"
+			source = gets.chomp
+			time = Time.now
+			record_transaction(db, user_name, expense, source, time, false)
+			puts "Here is your new current balance:"
+			puts add_transaction(db, user_name, expense, false)
+		end
 	elsif desired_task == 4
 		puts "How many transactions would you like to view? (Type a number ex: 1, 2, 3 or type 0 for all)"
 		number = gets.to_i
@@ -88,5 +114,3 @@ while desired_task != 9
 	spacing
 end
 puts "Thank you for using the spending app!"
-
-# Program can print all transactions, or requested number of transactions
