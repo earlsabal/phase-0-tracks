b0bfb7733717a38e5df3675a66325b1c2d18b34c
Earl Sabal <earlsabal@gmail.com>
2017-04-11 17:46:19 -0700

    Added pairing files

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,16 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+  "633 Folsom, San Francisco, CA 94107"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
